628af654c641ef450ffeb15725a34afa6b6c3f5d
ramishshah93 <ramishshah93@gmail.com>
2023-04-03 21:33:21 +0200

    wiring add to cart so it shows on cart icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Cart } from "./models/cart.model";
 
 @Component({
   selector: "app-root",
   template: `<app-header></app-header> <router-outlet></router-outlet>`,
   styles: [],
 })
-export class AppComponent {
-  title = "ecomStore";
+export class AppComponent implements OnInit {
+  cart: Cart = { items: [] };
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
