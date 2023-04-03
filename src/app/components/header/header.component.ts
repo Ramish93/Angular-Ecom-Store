74476844e32b6d2c44e5972ee9d7d5f86b01bb0e
ramishshah93 <ramishshah93@gmail.com>
2023-04-03 11:46:16 +0200

    added remove everything from cart method to cart service, wiring add to cart so it shows on cart icon next

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { Cart, CartItem } from "src/app/models/cart.model";
+import { CartService } from "src/app/services/cart.service";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private _cart: Cart = { items: [] };
+  itemsQuantity = 0;
+
+  constructor(private cartService: CartService) {}
+
+  @Input()
+  get cart(): Cart {
+    return this._cart;
+  }
+
+  set cart(cart: Cart) {
+    this._cart = cart;
+    this.itemsQuantity = cart.items
+      .map((item) => item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart() {
+    this.cartService.clearCart();
+  }
+}
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -24,6 +24,16 @@ export class CartService {
 
     this.cart.next({ items });
     this._snackBar.open("1 Item added to cart.", "Ok", { duration: 3000 });
-    console.log(this.cart.value);
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+
+  clearCart(): void {
+    this.cart.next({ items: [] });
+    this._snackBar.open("Cart is cleared.", "Ok", { duration: 2000 });
   }
 }
