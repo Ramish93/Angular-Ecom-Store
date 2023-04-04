52134c045d31dc2f278d8650953ec7f32e025b7f
ramishshah93 <ramishshah93@gmail.com>
2023-04-04 14:02:33 +0200

    function to get all products done, saving it with products state

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { Product } from "src/app/models/product.model";
 import { CartService } from "src/app/services/cart.service";
+import { StoreService } from "src/app/services/store.service";
 
 const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 450 };
 @Component({
   selector: "app-home",
   templateUrl: `./home.component.html`,
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   cols = 3;
   rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
+  products: Array<Product> | undefined;
+  sort = "desc";
+  count = "12";
+  productsSubsscription: Subscription | undefined;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private storeService: StoreService
+  ) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productsSubsscription = this.storeService
+      .getAllProducts(this.count, this.sort)
+      .subscribe((_products) => {
+        this.products = _products;
+      });
+  }
 
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Product } from "../models/product.model";
+
+const STORE_BASE_URL = "https://fakestoreapi.com";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class StoreService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getAllProducts(limit = "12", sort = "desc"): Observable<Array<Product>> {
+    return this.httpClient.get<Array<Product>>(
+      `${STORE_BASE_URL}/products?sort=${sort}&limit=${limit}`
+    );
+  }
 }
