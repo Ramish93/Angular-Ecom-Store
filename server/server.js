1741eb08d1368ddc3c13d3528f853211efa8ca1b
ramishshah93 <ramishshah93@gmail.com>
2023-04-05 00:47:32 +0200

    server.js file passing data to stript with post req

--- server/server.js
+++ server/server.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+const bodyparser = require("body-parser");
 
 const app = express();
-app;
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cors({ origin: true, credentials: true }));
+
+const stripe = require("stripe")("");
+
+app.post("/checkout", async (req, res, next) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: req.body.items.map((item) => ({
+        currency: "sek",
+        product_data: {
+          name: item.name,
+          images: [item.product],
+        },
+        unit_amount: item.price,
+      })),
+      mode: "payment",
+      success_url: "http://localhost:4242/success.html",
+      cancel_url: "http://localhost:4242/cancel.html",
+    });
+
+    res.status(200).json(session);
+  } catch (error) {
+    next(error);
+  }
+});
